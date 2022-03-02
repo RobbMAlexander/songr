54a93b955a0a6e59f2ad6dee91d9adf5ca47ef1c
Robb Alexander <robbmalexander@gmail.com>
2022-03-01 20:30:29 -0800

    lab 12 refactors-- CRUD reqs remain

--- src/main/java/com/robbmalexander/songr/controller/SongrController.java
+++ src/main/java/com/robbmalexander/songr/controller/SongrController.java
@@ -1,15 +1,20 @@
 package com.robbmalexander.songr.controller;
 
+import com.robbmalexander.songr.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Locale;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SongrController {
+
+    @Autowired
+    SongrRepository songrRepository;
 
     @ResponseBody
     @GetMapping("hello")
@@ -26,8 +31,19 @@ public class SongrController {
     @GetMapping ("albums")
     public String renderSplashPage (Model songrSplashModel) {
         //TODO: add attributes here as necessary
-        // songrSplashmodel.addAttribute( , );
+        //songrSplashmodel.addAttribute("albums", albums);
         return "splash-page.html";
     }
+
+/*    @ResponseBody
+    @PostMapping("/addAlbum")
+    public RedirectView addAlbum (String addedAlbumTitle, String addedAlbumArtist){
+        Album album = new Album(addedAlbumTitle, )
+                //TODO: write redirect for POST
+        return "Successfully added album: " + addedAlbumTitle;
+    }
+
+    @GetMapping("/")
+    public String get */
 
 }
---
+++ src/main/java/com/robbmalexander/songr/repository/AlbumRepository.java
@@ -0,0 +1,10 @@
+package com.robbmalexander.songr.repository;
+
+import com.robbmalexander.songr.model.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+    public Album findByTitle (String title);
+
+}
